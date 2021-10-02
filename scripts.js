514cc90aa9a270fce51677c1d2fc932d21b0b850
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2021-10-02 18:56:22 -0300

    adicionada a tabela no dashboard

--- scripts.js
+++ scripts.js
@@ -4,6 +4,7 @@ const countPlanets = document.getElementById("planetas");
 const countStarships = document.getElementById("naves");
 
 fillCount();
+fillTable();
 
 function fillCount() {
   // countPeople.innerHTML = swapiGet("people/").count;
@@ -22,4 +23,24 @@ function fillCount() {
 
 function swapiGet(param) {
   return axios.get(`https://swapi.dev/api/${param}`);
+}
+
+async function fillTable(){
+  const response = await swapiGet("films/");
+  const tableData = response.data.results;
+  console.log(tableData);
+
+  let contentTable = ``
+
+  for(let i=0; i<tableData.length; i++){
+    contentTable = contentTable + `<tr>
+    <td>${tableData[i].title}</td>
+    <td>${moment(tableData[i].release_date).format("DD/MM/YYYY")}</td>
+    <td>${tableData[i].director}</td>
+    <td>${tableData[i].episode_id}</td>
+    </tr>`    
+  }
+  console.log(contentTable)
+  document.getElementById("tbody")
+  .innerHTML = contentTable
 }
